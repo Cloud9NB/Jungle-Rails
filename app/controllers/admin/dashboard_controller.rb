396e4ab9fd3250de0699dc273c1e96ea6b980a00
Kevin Lee <blackseeds10@hotmail.com>
2022-05-04 21:59:32 +0000

    add a feature for admin dahsboard where it counts the products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASS"]
 
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
