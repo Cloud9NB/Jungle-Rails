dce06304cdef1a93e8b3330da0a07bc936e138ed
Kevin Lee <blackseeds10@hotmail.com>
2022-05-05 03:12:54 +0000

    add an admin categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+  end
   get 'about/index'
   get 'static_pages/help'
   get 'static_pages/faq'
@@ -19,6 +24,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :delete]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
