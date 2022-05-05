8e933393dfd32445160ce888cc82070267b19d2c
Kevin Lee <blackseeds10@hotmail.com>
2022-05-05 22:38:56 +0000

    reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,46 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  namespace :admin do
-    get 'categories/index'
-    get 'categories/new'
-    get 'categories/create'
-  end
-  get 'about/index'
-  get 'static_pages/help'
-  get 'static_pages/faq'
-  get 'static_pages/about'
-  root to: 'products#index'
-
-  resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
-
-  resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
-  end
-
-  resources :orders, only: [:create, :show]
-
-  namespace :admin do
-    root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show, :delete]
-  end
-
+  
   # these routes are for showing users a login form, logging them in, and logging them out.
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  
+
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  
+  get 'about/index'
+  get 'static_pages/help'
+  get 'static_pages/faq'
+  get 'static_pages/about'
+    
+  root to: 'products#index'
+  
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
+  
+  resource :cart, only: [:show] do
+    post   :add_item
+    post   :remove_item
+  end
+  
+  resources :orders, only: [:create, :show]
+  
+  namespace :admin do
+    root to: 'dashboard#show'
+    get 'categories/index'
+    get 'categories/new'
+    get 'categories/create'
+    resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :delete]
+  end
+
+  
 
 
   # The priority is based upon order of creation: first created -> highest priority.
